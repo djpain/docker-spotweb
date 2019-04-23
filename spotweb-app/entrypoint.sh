058df1933748770932faf036dec294c083d4cb2a
Alex Eduardo Painemilla <apainemilla@assemblypayments.com>
2019-04-23 20:52:47 +1000

    changed from apache 2.4 to nginx/php-fpm

--- spotweb-app/entrypoint.sh
+++ spotweb-app/entrypoint.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/ash
 
-if [ ! -f /config/ownsettings.php ] && [ -f /var/www/spotweb/ownsettings.php ]; then
-  cp /var/www/spotweb/ownsettings.php /config/ownsettings.php
+if [ ! -f /config/ownsettings.php ] && [ -f /var/www/html/spotweb/ownsettings.php ]; then
+  cp /var/www/html/spotweb/ownsettings.php /config/ownsettings.php
 fi
 
 touch /config/ownsettings.php && chown www-data:www-data /config/ownsettings.php
-rm -f /var/www/spotweb/ownsettings.php
-ln -s /config/ownsettings.php /var/www/spotweb/ownsettings.php
+rm -f /var/www/html/spotweb/ownsettings.php
+ln -s /config/ownsettings.php /var/www/html/spotweb/ownsettings.php
 
 chown -R www-data:www-data /var/www/spotweb
 
@@ -22,35 +22,36 @@ fi
 
 if [ -f /config/dbsettings.inc.php ]; then
 	chown www-data:www-data /config/dbsettings.inc.php
-	rm /var/www/spotweb/dbsettings.inc.php
-	ln -s /config/dbsettings.inc.php /var/www/spotweb/dbsettings.inc.php
+	rm /var/www/html/spotweb/dbsettings.inc.php
+	ln -s /config/dbsettings.inc.php /var/www/html/spotweb/dbsettings.inc.php
 else
 	echo -e "\nWARNING: You have no database configuration file, either create /config/dbsettings.inc.php or restart this container with the correct environment variables to auto generate the config.\n"
 fi
 
 TZ=${TZ:-"Australia/Sydney"}
 echo -e "Setting (PHP) time zone to ${TZ}\n"
-sed -i "s#^;date.timezone =.*#date.timezone = ${TZ}#g"  /etc/php/7.*/*/php.ini
+#sed -i "s#^;date.timezone =.*#date.timezone = ${TZ}#g"  /etc/php/7.*/*/php.ini
 
 if [[ -n "$SPOTWEB_CRON_RETRIEVE" || -n "$SPOTWEB_CRON_CACHE_CHECK" ]]; then
     ln -sf /proc/$$/fd/1 /var/log/stdout
     service cron start
 	if [[ -n "$SPOTWEB_CRON_RETRIEVE" ]]; then
-        echo "$SPOTWEB_CRON_RETRIEVE su -l www-data -s /usr/bin/php /var/www/spotweb/retrieve.php >/var/log/stdout 2>&1" > /etc/crontab
+        echo "$SPOTWEB_CRON_RETRIEVE su -l www-data -s /usr/bin/php /var/www/html/spotweb/retrieve.php >/var/log/stdout 2>&1" > /etc/crontab
 	fi
 	if [[ -n "$SPOTWEB_CRON_CACHE_CHECK" ]]; then
-        echo "$SPOTWEB_CRON_CACHE_CHECK su -l www-data -s /usr/bin/php /var/www/spotweb/bin/check-cache.php >/var/log/stdout 2>&1" >> /etc/crontab
+        echo "$SPOTWEB_CRON_CACHE_CHECK su -l www-data -s /usr/bin/php /var/www/html/spotweb/bin/check-cache.php >/var/log/stdout 2>&1" >> /etc/crontab
 	fi
     crontab /etc/crontab
 fi
 
-# Run database update
-/usr/bin/php /var/www/spotweb/bin/upgrade-db.php >/dev/null 2>&1
+# # Run database update
+# /usr/bin/php /var/www/spotweb/bin/upgrade-db.php >/dev/null 2>&1
 
-# Clean up apache pid (if there is one)
-rm -rf /run/apache2/apache2.pid
+# # Clean up apache pid (if there is one)
+# rm -rf /run/apache2/apache2.pid
 
-# Enabling PHP mod rewrite
-/usr/sbin/a2enmod rewrite && /etc/init.d/apache2 restart
+# # Enabling PHP mod rewrite
+# /usr/sbin/a2enmod rewrite && /etc/init.d/apache2 restart
 
-tail -F /var/log/apache2/* /dev/stdout /dev/stderr
+# tail -F /var/log/apache2/* /dev/stdout /dev/stderr
+php-fpm
